4012a4f5e53dfb74e2a70e5492fedb6575e56970
Felipe Coutinho Castilho <castfelipe10@gmail.com>
2025-08-09 22:18:11 -0300

    header site

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//DROPDOWN MENU
+const dropdownMenu = document.getElementById("dropdown-item");
+const menuItem = document.getElementById("menu-item");
+
+menuItem.addEventListener("mouseenter", () => {
+  dropdownMenu.classList.add("active-dropdown-menu");
+});
+
+menuItem.addEventListener("mouseleave", () => {
+  dropdownMenu.classList.remove("active-dropdown-menu");
+});
+
+// MENU MOBILE
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("active-menu-mobile");
+      toggle.classList.toggle("active-bx");
+    });
+  }
+};
+
+showMenu("bx", "menu-mobile");
