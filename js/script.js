217364b46213ae9bbf3c6e69568bbbe31a01b675
Felipe Coutinho Castilho <castfelipe10@gmail.com>
2025-08-14 20:23:25 -0300

    adicionando conteudo no hero

--- js/script.js
+++ js/script.js
@@ -25,3 +25,14 @@ const showMenu = (toggleId, navId) => {
 };
 
 showMenu("bx", "menu-mobile");
+
+// SCROLL HEADER
+
+function scrollHeader() {
+  const nav = document.getElementById("header");
+
+  if (this.scrollY >= 50) nav.classList.add("active-header");
+  else nav.classList.remove("active-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
