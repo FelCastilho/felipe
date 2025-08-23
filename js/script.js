01f41fdbc7c90a017d61d27c67feefffd842c720
Felipe Coutinho Castilho <castfelipe10@gmail.com>
2025-08-23 10:42:17 -0300

    adicionado gsap

--- js/script.js
+++ js/script.js
@@ -39,14 +39,104 @@ window.addEventListener("scroll", scrollHeader);
 
 // GSAP ANIMATIONS
 
-
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.from(".navbar-desktop", {
+  y: -50,
+  opacity: 0,
+  delay: 1,
+  duration: 1,
+  ease: "power1.out",
+});
 
 gsap.from("#hero-title", {
   y: -50,
   opacity: 0,
   duration: 1,
-  ease: "power2.out",
+  ease: "power1.out",
 });
+
+gsap.from("#hero-text", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  ease: "power1.out",
+});
+
+gsap.fromTo(
+  ".btn-hero",
+  {
+    opacity: 0,
+    duration: 1,
+    delay: 1.5,
+    ease: "power1.out",
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power1.out",
+  }
+);
+
+gsap.fromTo(".container-title", {
+  y: -50,
+  opacity: 0,
+},{
+  y: 0,
+  opacity: 1,
+  scrollTrigger:{
+    trigger: ".container-title",
+    start: "top 70%",
+    end: "bottom 20%",
+    scrub: true,
+
+  }
+})
+
+gsap.fromTo("#mcco", {
+  x: -50,
+  opacity: 0,
+},{
+  x: 0,
+  opacity: 1,
+  scrollTrigger:{
+    trigger: "#mcco",
+    start: "top 90%",
+    end: "bottom 10%",
+    scrub: true,
+    markers: true,
+  }
+})
+
+gsap.fromTo("#mariana-solano", {
+  x: 50,
+  opacity: 0,
+},{
+  x: 0,
+  opacity: 1,
+  scrollTrigger:{
+    trigger: "#mariana-solano",
+    start: "top 90%",
+    end: "bottom 100%",
+    scrub: true,
+    markers: true,
+  }
+})
+
+gsap.fromTo("#gelcamp-engenharia", {
+  x: -50,
+  opacity: 0,
+},{
+  x: 0,
+  opacity: 1,
+  scrollTrigger:{
+    trigger: "#gelcamp-engenharia",
+    start: "top 90%",
+    end: "bottom 100%",
+    scrub: true,
+    markers: true,
+  }
+})
+
 
